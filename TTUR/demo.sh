ffde92457d97bec759a695c1e676ca08cf02024d
crsm424 <104885545+crsm424@users.noreply.github.com>
2023-08-01 11:44:00 +0800

    Add files via upload

---
+++ TTUR/demo.sh
@@ -0,0 +1,5 @@
+python fid.py ../Market/pytorch/train_all ../Market/pytorch/gallery --gpu 2
+#python fid.py ../Market/pytorch/train_all ../GAN_results/PG-GAN --gpu 2
+#python fid.py ../Market/pytorch/train_all ../GAN_results/FD-GAN --gpu 2
+#python fid_score.py ../Market/pytorch/train_all ../Market/pytorch/off-gan_id1 --gpu 0
+#python fid_score.py ../Market/pytorch/train_all ../cloud/Market/pytorch/gan_id-noloop --gpu 0
